413395b28529a766410565d495928372c9bcd954
Miguel Obando <miguel.obando@s4e.cl>
2021-08-12 23:34:02 -0400

    Scrapper commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require('axios');
+const sqlite3 = require('sqlite3');
+const url = 'https://dashboard.elering.ee//api/nps/price/EE/current';
+let db = new sqlite3.Database('./db/prices.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the DB');
+  });
+  
+  let scraper = () =>{ 
+    axios.get(url)
+        .then((response) => {
+            let unix_date = response.data.data[0].timestamp;
+            let date = new Date(unix_date * 1000);
+            let hours = date.getHours();
+            let minutes = "0" + date.getMinutes();
+            let seconds = "0" + date.getSeconds();
+            let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+            let price = response.data.data[0].price; 
+            db.run(`INSERT INTO  energy_price (time,prices) VALUES('${formattedTime}',${price});`);
+            console.log( formattedTime ," price: ",price );
+        }).catch((err) => {
+            console.log(err);
+        });
+    
+};
+
+setInterval(() => {
+    scraper();    
+}, 3600000);
+ 
+
